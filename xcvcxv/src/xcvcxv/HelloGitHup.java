a762f223a0b7eea099784314488f35adcd6a9bcf
UID <UID@DESKTOP-DACJOCN>
2019-05-07 11:09:17 +0800

    23

--- xcvcxv/src/xcvcxv/HelloGitHup.java
+++ xcvcxv/src/xcvcxv/HelloGitHup.java
@@ -3,6 +3,6 @@ package xcvcxv;
 public class HelloGitHup {
 	public static void main(String[] args) {
 		System.out.println("hello123 !!!");
-		System.out.println("Сpeipei");
+		System.out.println("小peipei123");
 	}
 }
