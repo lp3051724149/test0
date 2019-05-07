13a564de662a18ae471aa7fe346d84124d398071
UID <UID@DESKTOP-DACJOCN>
2019-05-07 11:29:04 +0800

    cfhbg

--- sdfsdaf/src/main/java/sdfsdaf/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package sdfsdaf;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("hello githup!!!");
-		System.out.println("two down!!!!!!!!!!!!");
-	}
-}
